f477cfd5e59ca68e755fca99dab962a20a8015db
J Miles <j.milesy.nz@gmail.com>
2017-05-11 17:50:37 +1200

    Add get_point_on to Cube to allow easy location of corners/faces

--- lib/solidruby/primitives/cube.rb
+++ lib/solidruby/primitives/cube.rb
@@ -55,6 +55,102 @@ module SolidRuby::Primitives
       @center
     end
 
+    def get_point_on(args = {})
+      face = args[:face] || :top
+      edge = args[:edge] || :center
+      corner = args[:corner] || :center
+
+      vert_axis = :z
+      horiz_axis = :x
+      vert_dir = 1
+      horiz_dir = 1
+
+      pos = case face
+      when :top
+        vert_axis = :y
+        horiz_dir = -1
+        {x: @x / 2.0, y: @y / 2.0, z: @z }
+      when :bottom
+        vert_axis = :y
+        horiz_dir = -1
+        vert_dir = -1
+        { x: @x / 2.0, y: @y / 2.0, z: 0 }
+      when :left
+        horiz_axis = :y
+        { x: 0, y: @y / 2.0, z: @z / 2.0 }
+      when :right
+        horiz_axis = :y
+        horiz_dir = -1
+        { x: @x, y: @y / 2.0, z: @z / 2.0 }
+      when :front
+        horiz_dir = -1
+        { x: @x / 2.0, y: 0, z: @z / 2.0 }
+      when :back
+        { x: @x / 2.0, y: @y, z: @z / 2.0 }
+      when :center
+        horiz_dir = -1
+        { x: @x / 2.0, y: @y / 2.0, z: @z / 2.0 }
+      else
+        nil
+      end
+
+      return pos if pos.nil?
+      is_centered = {x: @center, y: @center, z: @center}
+
+      unless @center || @transformations.first.nil?
+        is_centered[:x] = @transformations.first.x == (-@x / 2.0)
+        is_centered[:y] = @transformations.first.y == (-@y / 2.0)
+        is_centered[:z] = @transformations.first.z == (-@z / 2.0)
+      end
+
+      pos[:x] -= @x / 2.0 if is_centered[:x]
+      pos[:y] -= @y / 2.0 if is_centered[:y]
+      pos[:z] -= @z / 2.0 if is_centered[:z]
+
+      #pos is now center of the given face, move to the given side
+      h_change = case horiz_axis
+      when :x
+        @x / 2.0
+      when :y
+        @y / 2.0
+      else
+        @z / 2.0
+      end
+
+      v_change = case vert_axis
+      when :x
+        @x / 2.0
+      when :y
+        @y / 2.0
+      else
+        @z / 2.0
+      end
+
+      case edge
+      when :top
+        pos[vert_axis] += (v_change * vert_dir)
+      when :bottom
+        pos[vert_axis] -= (v_change * vert_dir)
+      when :left
+        pos[horiz_axis] += (h_change * horiz_dir)
+      when :right
+        pos[horiz_axis] -= (h_change * horiz_dir)
+      end
+
+      case corner
+      when :top
+        pos[vert_axis] += (v_change * vert_dir)
+      when :bottom
+        pos[vert_axis] -= (v_change * vert_dir)
+      when :left
+        pos[horiz_axis] += (h_change * horiz_dir)
+      when :right
+        pos[horiz_axis] -= (h_change * horiz_dir)
+      end
+
+      pos
+    end
+
     def to_rubyscad
       args = { size: [@x, @y, @z] }
       args[:center] = @center if @center
--- test/primitives/cube_test.rb
+++ test/primitives/cube_test.rb
@@ -48,4 +48,79 @@ class CubeTest < Minitest::Test
     end
   end
 
+  def test_cube_get_point_on
+    cube_args = {x: 10, y: 20, z: 30}
+    vals = {
+      #6 faces
+      {face: :top, edge: :center, corner: :center} => { x: 5, y: 10, z: 30 },
+      {face: :bottom, edge: :center, corner: :center} => { x: 5, y: 10, z: 0 },
+      {face: :left, edge: :center, corner: :center} => { x: 0, y: 10, z: 15 },
+      {face: :right, edge: :center, corner: :center} => { x: 10, y: 10, z: 15 },
+      {face: :front, edge: :center, corner: :center} => { x: 5, y: 0, z: 15 },
+      {face: :back, edge: :center, corner: :center} => { x: 5, y: 20, z: 15 },
+
+      #4 edges (top)
+      {face: :top, edge: :left, corner: :center} => { x: 0, y: 10, z: 30 },
+      {face: :top, edge: :right, corner: :center} => { x: 10, y: 10, z: 30 },
+      {face: :top, edge: :top, corner: :center} => { x: 5, y: 20, z: 30 },
+      {face: :top, edge: :bottom, corner: :center} => { x: 5, y: 0, z: 30 },
+
+      #4 corners (top)
+      {face: :top, edge: :left, corner: :top} => { x: 0, y: 20, z: 30 },
+      {face: :top, edge: :right, corner: :bottom} => { x: 10, y: 0, z: 30 },
+      {face: :top, edge: :top, corner: :left} => { x: 0, y: 20, z: 30 },
+      {face: :top, edge: :bottom, corner: :right} => { x: 10, y: 0, z: 30 },
+
+      #4 corners (left)
+      {face: :left, edge: :left, corner: :top} => { x: 0, y: 20, z: 30 },
+      {face: :left, edge: :right, corner: :bottom} => { x: 0, y: 0, z: 0 },
+      {face: :left, edge: :top, corner: :right} => { x: 0, y: 0, z: 30 },
+      {face: :left, edge: :bottom, corner: :left} => { x: 0, y: 20, z: 0 },
+
+      #4 corners (right)
+      {face: :right, edge: :left, corner: :top} => { x: 10, y: 0, z: 30 },
+      {face: :right, edge: :right, corner: :bottom} => { x: 10, y: 20, z: 0 },
+      {face: :right, edge: :top, corner: :right} => { x: 10, y: 20, z: 30 },
+      {face: :right, edge: :bottom, corner: :left} => { x: 10, y: 0, z: 0 },
+
+      #4 corners (back)
+      {face: :back, edge: :left, corner: :top} => { x: 10, y: 20, z: 30 },
+      {face: :back, edge: :right, corner: :bottom} => { x: 0, y: 20, z: 0 },
+      {face: :back, edge: :top, corner: :right} => { x: 0, y: 20, z: 30 },
+      {face: :back, edge: :bottom, corner: :left} => { x: 10, y: 20, z: 0 },
+    }
+
+    vals.each do |val, exp|
+      val = Cube.new(cube_args).get_point_on(val)
+      assert_equal exp, val
+    end
+
+    #check x/y centered cube
+    vals = {
+      #4 corners (top)
+      {face: :top, edge: :left, corner: :top} => { x: -5, y: 10, z: 30 },
+      {face: :top, edge: :right, corner: :bottom} => { x: 5, y: -10, z: 30 },
+      {face: :top, edge: :top, corner: :right} => { x: 5, y: 10, z: 30 },
+      {face: :top, edge: :bottom, corner: :left} => { x: -5, y: -10, z: 30 },
+    }
+
+    vals.each do |val, exp|
+      val = Cube.new(cube_args).center_xy.get_point_on(val)
+      assert_equal exp, val
+    end
+
+    #check x/y/z centered cube
+    vals = {
+      #4 corners (top)
+      {face: :left, edge: :left, corner: :top} => { x: -5, y: 10, z: 15 },
+      {face: :right, edge: :right, corner: :bottom} => { x: 5, y: 10, z: -15 },
+      {face: :top, edge: :top, corner: :right} => { x: 5, y: 10, z: 15 },
+      {face: :top, edge: :bottom, corner: :left} => { x: -5, y: -10, z: 15 },
+    }
+
+    vals.each do |val, exp|
+      val = Cube.new(cube_args).center.get_point_on(val)
+      assert_equal exp, val
+    end
+  end
 end
