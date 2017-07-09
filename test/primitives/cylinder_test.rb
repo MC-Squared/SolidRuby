ceef9ee7a05fe805908649cb5ab894079022b0b1
J Miles <j.milesy.nz@gmail.com>
2017-07-09 17:00:13 +1200

    Move cylinder and sphere tests to their own files

---
+++ test/primitives/cylinder_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class CylinderTest < Minitest::Test
+  def test_cylinder_scad
+    vals = {
+      Cylinder.new(h: 1) => 'cylinder(h = 1);',
+      Cylinder.new(r: 2) => 'cylinder(r = 2);',
+      Cylinder.new(d: 3) => 'cylinder(r = 1.500);',
+      Cylinder.new(h: 2, r: 3) => 'cylinder(h = 2, r = 3);',
+      Cylinder.new(h: 1, r: 2, d: 3) => 'cylinder(h = 1, r = 1.500);',
+      Cylinder.new(h: 20, d: 15, fn: 20) => 'cylinder(h = 20, $fn = 20, r = 7.500);',
+      Cylinder.new(q: 5) => 'cylinder(q = 5);'
+    }
+
+    vals.each do |val, exp|
+      assert_equal exp, val.to_rubyscad
+    end
+  end
+
+  def test_cylinder_helper
+    vals = {
+      cylinder(h: 1) => 'cylinder(h = 1);',
+      cylinder(r: 2) => 'cylinder(r = 2);',
+      cylinder(d: 3) => 'cylinder(r = 1.500);',
+      cylinder(h: 2, r: 3) => 'cylinder(h = 2, r = 3);',
+      cylinder(h: 1, r: 2, d: 3) => 'cylinder(h = 1, r = 1.500);',
+      cylinder(h: 10, d: 20, fn: 15) => 'cylinder(h = 10, $fn = 15, r = 10.000);',
+      cylinder(q: 15) => 'cylinder(q = 15);',
+
+      cylinder(id: 10, h: 10) =>
+        "difference(){cylinder(h = 10);\n" \
+        "translate(v = [0, 0, -0.010])\n" \
+        "cylinder(h = 10.020, r = 5.000);\n"\
+        '}',
+
+      cylinder(id: 10, h: 12, ih: 10) =>
+          "difference(){cylinder(h = 12);\n" \
+          "translate(v = [0, 0, -0.010])\n" \
+          "cylinder(h = 10.010, ih = 10, r = 5.000);\n"\
+          '}',
+
+      cylinder(id: 10, h: 12, ih: 10, ifn: 20) =>
+            "difference(){cylinder(h = 12);\n" \
+            "translate(v = [0, 0, -0.010])\n" \
+            "cylinder(h = 10.010, ih = 10, ifn = 20, $fn = 20, r = 5.000);\n"\
+            '}'
+    }
+
+    vals.each do |val, exp|
+      assert_equal exp, val.to_rubyscad
+    end
+  end
+end
---
+++ test/primitives/sphere_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class SphereTest < Minitest::Test
+  def test_sphere_scad
+    vals = {
+      Sphere.new(r: 10) => 'sphere(r = 10);',
+      Sphere.new(d: 15) => 'sphere(r = 7.500);',
+      Sphere.new(q: 5, r: 10) => 'sphere(q = 5, r = 10);',
+      Sphere.new(q: 5, r: 10, center: true) => 'sphere(q = 5, r = 10, center = true);'
+    }
+
+    vals.each do |val, exp|
+      assert_equal exp, val.to_rubyscad
+    end
+  end
+
+  def test_sphere_helper
+    vals = {
+      sphere(r: 10) => 'sphere(r = 10);',
+      sphere(d: 15) => 'sphere(r = 7.500);',
+      sphere(q: 5, r: 10) => 'sphere(q = 5, r = 10);',
+      sphere(q: 5, r: 10, center: true) => 'sphere(q = 5, r = 10, center = true);'
+    }
+
+    vals.each do |val, exp|
+      assert_equal exp, val.to_rubyscad
+    end
+  end
+end
